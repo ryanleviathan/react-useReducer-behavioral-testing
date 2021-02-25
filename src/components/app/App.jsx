b31e39eded0a7e97af3317dc4148892a1bdc9b50
ryanleviathan <ryan.m.carreras@gmail.com>
2021-02-25 11:08:38 -0800

    colorReducer

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,35 +1,5 @@
-import React, { useState, useReducer } from 'react';
+import React, { useReducer } from 'react';
 import reducer, { initialState } from '../../reducers/colorReducer';
-
-// const useRecord = (init) => {
-//   const [before, setBefore] = useState([]);
-//   const [current, setCurrent] = useState(init);
-//   const [after, setAfter] = useState([]);
-
-//   const undo = () => {
-//     setAfter(after => [current, ...after]);
-//     setCurrent(before[before.length - 1]);
-//     setBefore(before => before.slice(0, -1));
-//   };
-
-//   const redo = () => {
-//     setBefore(before => [...before, current]);
-//     setCurrent(after[0]);
-//     setAfter(after => after.slice(1));
-//   }
-
-//   const record = val => {
-//     setBefore(before => [...before, current]);
-//     setCurrent(val);
-//   }
-
-//   return {
-//     undo,
-//     record,
-//     redo,
-//     current,
-//   }
-// };
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -43,12 +13,29 @@ function App() {
 
   return (
     <>
-      <button onClick={undo}>undo</button>
-      <button onClick={redo}>redo</button>
-      <input type="color" value={current} onChange={({ target }) => record(target.value)} />
-      <div style=></div>
+      <button 
+        id="UNDO" 
+        disabled={!state.before.length} 
+        onClick={handleChange}>
+          undo
+      </button>
+      <button 
+        id="REDO" 
+        disabled={!state.after.length} 
+        onClick={handleChange}>
+          redo
+      </button>
+      <input 
+        id="INPUT" 
+        type="color" 
+        value={state.current} 
+        onChange={handleChange} />
+      <div 
+        data-testid="display" 
+        style={{ backgroundColor: state.current }}>
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
